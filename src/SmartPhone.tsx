a4be0776f376f745c1c24c7f3f603bd085db2469
RabbitProgram <rabbitprogram.2018@gmail.com>
2023-02-02 23:18:19 +0900

    feat: スマホ用ページを適用

--- src/SmartPhone.tsx
+++ src/SmartPhone.tsx
@@ -1,11 +1,20 @@
 import { Box } from "@mui/material";
 import "./App.css";
+import animal_chara_computer_penguin from "./animal_chara_computer_penguin.png";
 
 export const SmartPhone = () => {
   return (
     <Box className="App">
       <Box className="App-header" sx={{ backgroundColor: "#424242" }}>
-        パソコンでご覧ください
+        <img
+          src={animal_chara_computer_penguin}
+          className="TopImage"
+          alt="TopImage"
+        />
+        <div style={{ height: 15 }} />
+        PCでアクセスしてください
+        <br />
+        https://is.gd/urlomkj
       </Box>
     </Box>
   );
